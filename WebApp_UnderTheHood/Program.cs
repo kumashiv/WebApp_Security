d91869a4ee021b36d1475424a8e2d87bc46e0df6
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-11 20:47:08 -0400

    Policy added - program.cs
    Razor Pages added
    - HumanResource,
    - Access Denied
    
    Login but won’t be able to go to HumanResource page because of additional policy requirement.

--- WebApp_UnderTheHood/Program.cs
+++ WebApp_UnderTheHood/Program.cs
@@ -6,6 +6,13 @@ builder.Services.AddRazorPages();
 builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
 {
     options.Cookie.Name = "MyCookieAuth";
+    //options.LoginPath = "/Account/Login";
+});
+
+// Add policy
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBelongToHRDepartment", policy => policy.RequireClaim("Department", "HR"));
 });
 
 var app = builder.Build();

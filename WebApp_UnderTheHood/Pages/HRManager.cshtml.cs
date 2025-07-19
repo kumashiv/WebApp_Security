327910fc8669ff6dc001993d9baca9db0ce1bd3c
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-18 20:51:26 -0400

    Step 1: - WebApp > Http.Extensions package for HttpClientFactory - Add API as client - Uri address
    Step 2:
    - Add HRManager as Http Client to trigger Web API
    - WebApp > DTO/WeatherForecastDTO.cs

--- WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
+++ WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
@@ -1,14 +1,25 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp_UnderTheHood.DTO;
 
 namespace WebApp_UnderTheHood.Pages
 {
     [Authorize(Policy = "HRManagerOnly")]
     public class HRManagerModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public HRManagerModel(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        // Http client to trigger WebAPI
+        public async Task OnGetAsync()
+        {
+            var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+            httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast");
         }
     }
 }

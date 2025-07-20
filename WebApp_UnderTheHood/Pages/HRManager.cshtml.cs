189f58bfc3916784825d90f0582021f388c32b44
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-19 23:21:05 -0400

    Consume the Endpoint protected by JWT Token
    Sending request with credentials and token
    
    WebApp
    - HRManager.cshtml.cs
    - Authorization/JwtToken.cs

--- WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
+++ WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
@@ -1,7 +1,12 @@
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using WebApp_UnderTheHood.Authorization;
 using WebApp_UnderTheHood.DTO;
+using WebApp_UnderTheHood.Pages.Account;
 
 namespace WebApp_UnderTheHood.Pages
 {
@@ -22,6 +27,15 @@ namespace WebApp_UnderTheHood.Pages
         public async Task OnGetAsync()
         {
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+
+            //Post request with credentials to API endpoint/ Auth 
+            var res = await httpClient.PostAsJsonAsync("auth", new Credential { UserName = "admin", Password = "123" });
+            res.EnsureSuccessStatusCode();
+            string strJwt = await res.Content.ReadAsStringAsync();      // This is JWT token
+            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);       //JwtToken from Authorization/JwtToken   // deserialize strJwt into an object
+
+            // Sending token in Header (Bearer is scheme name)
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken??string.Empty);    // Bearer is the scheme name
             weatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast")??new List<WeatherForecastDTO>();     //new List<WeatherForecastDTO>() is to avoid null reference warning
         }
     }

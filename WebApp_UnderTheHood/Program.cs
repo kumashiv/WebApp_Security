15051192f730c6334b8a616ffc3c3bbb402ec339
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-20 14:28:28 -0400

    WebApp/ program.cs - Add session and Session Middleware

--- WebApp_UnderTheHood/Program.cs
+++ WebApp_UnderTheHood/Program.cs
@@ -32,6 +32,14 @@ builder.Services.AddHttpClient("OurWebAPI", client =>
     client.BaseAddress = new Uri("https://localhost:7158/");
 });
 
+// Configuring the behavior of our Cookie
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,6 +57,8 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSession();   // Session Middleware
 
 app.MapRazorPages();
 

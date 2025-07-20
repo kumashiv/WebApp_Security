4ea5f38e9397ab089638492aed2a461160fbd6b7
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-20 15:26:06 -0400

    WebApp
    HRMnager.cshtml.cs
    
    1. If we already have token stored in the session, get token from the session first
    2. If token is not found or has expired, create and authenticate and save it in session
    
    Repetitive code for authentication and getting the token, moved to new method
    private async Task<JwtToken> Authenticate ()
    
    Results:
    1. Logging in (WebApp) with credentials produces a authentication cookie – MyCookieAuth
    2. Accessing HRManager produces a Session cookie (Done via Token) - .AspNetCore.Session

--- WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
+++ WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
@@ -26,17 +26,48 @@ namespace WebApp_UnderTheHood.Pages
         // Http client to trigger WebAPI
         public async Task OnGetAsync()
         {
+            //If we already have token stored in the session, get token from the session first
+            JwtToken token = new JwtToken();        // If empty, initialize
+
+            var strTokenObj = HttpContext.Session.GetString("acces_token");
+            if (string.IsNullOrEmpty(strTokenObj))      // If no token in session yet
+            {
+                token = await Authenticate();
+            }
+            else   // If there is token in session
+            {
+                token = JsonConvert.DeserializeObject<JwtToken>(strTokenObj)?? new JwtToken();      // If empty, initialize - might produce null token
+            }
+
+            // If token is null or expired
+            if (token == null ||
+                string.IsNullOrWhiteSpace(token.AccessToken) ||
+                token.ExpiresAt <= DateTime.UtcNow)
+            {
+                token = await Authenticate();
+            }
+
+            var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+            // Sending token in Header (Bearer is scheme name)
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken ?? string.Empty);    // Bearer is the scheme name
+            weatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast")??new List<WeatherForecastDTO>();     //new List<WeatherForecastDTO>() is to avoid null reference warning
+        }
+
+        private async Task<JwtToken> Authenticate ()
+        {
+            //authentication and getting the token
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
 
             //Post request with credentials to API endpoint/ Auth 
             var res = await httpClient.PostAsJsonAsync("auth", new Credential { UserName = "admin", Password = "123" });
             res.EnsureSuccessStatusCode();
             string strJwt = await res.Content.ReadAsStringAsync();      // This is JWT token
-            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);       //JwtToken from Authorization/JwtToken   // deserialize strJwt into an object
 
-            // Sending token in Header (Bearer is scheme name)
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken??string.Empty);    // Bearer is the scheme name
-            weatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast")??new List<WeatherForecastDTO>();     //new List<WeatherForecastDTO>() is to avoid null reference warning
+            //	If we get the token for first time or after token is expired, we store it in session
+            HttpContext.Session.SetString("access_token", strJwt);
+
+            //return token
+            return JsonConvert.DeserializeObject<JwtToken>(strJwt)??new JwtToken();       //JwtToken from Authorization/JwtToken   // deserialize strJwt into an object
         }
     }
 }
